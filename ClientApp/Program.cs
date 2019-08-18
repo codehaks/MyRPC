20ec66d724b4d08c5735c443fc799c75cb608b45
Hakim Ghods <ghods.hakim@hotmail.com>
2019-08-18 17:01:51 +0430

    Add project files.

---
+++ ClientApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.IO;
+
+namespace ClientApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                TcpClient Tcpclient = new TcpClient();
+                Console.WriteLine("Connecting..");
+                Tcpclient.Connect("127.100.100.50", 3000);
+                Console.WriteLine("Connected");
+                Console.WriteLine("Ente the String you want to send ");
+                string str = Console.ReadLine();
+                Stream stm = Tcpclient.GetStream();
+                ASCIIEncoding ascnd = new ASCIIEncoding();
+                byte[] ba = ascnd.GetBytes(str);
+                Console.WriteLine("Sending..");
+                stm.Write(ba, 0, ba.Length);
+                byte[] bb = new byte[100];
+                int k = stm.Read(bb, 0, 100);
+                for (int i = 0; i < k; i++)
+                {
+                    Console.Write(Convert.ToChar(bb[i]));
+                }
+
+                Tcpclient.Close();
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error" + ex.StackTrace);
+            }
+        }
+    }
+}
---
+++ ServerApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.Net;
+
+namespace ServerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+       
+                IPAddress ipaddress = IPAddress.Parse("127.100.100.50");
+                TcpListener mylist = new TcpListener(ipaddress, 3000);
+                mylist.Start();
+                Console.WriteLine("Server is Running on Port: 3000");
+                Console.WriteLine("Local endpoint:" + mylist.LocalEndpoint);
+                Console.WriteLine("Waiting for Connections...");
+                Socket s = mylist.AcceptSocket();
+                Console.WriteLine("Connection Accepted From:" + s.RemoteEndPoint);
+                byte[] b = new byte[100];
+                int k = s.Receive(b);
+                Console.WriteLine("Recieved..");
+                for (int i = 0; i < k; i++)
+                {
+                    Console.Write(Convert.ToChar(b[i]));
+                }
+                ASCIIEncoding asencd = new ASCIIEncoding();
+                s.Send(asencd.GetBytes("Automatic Message:" + "String Received byte server !"));
+                Console.WriteLine("\nAutomatic Message is Sent");
+                s.Close();
+                mylist.Stop();
+                Console.ReadLine();
+           
+        }
+    }
+}
